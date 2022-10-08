d593136774801ccc1112a466d3596b1190632fb2
Юлия Алексина <ulchez@mail.ru>
2022-10-08 17:51:53 +0300

    Добавьте файлы проекта.

---
+++ Lab19WpfApp1/Models/Ariph1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WpfApp1.Models
+{
+    static class Ariph
+    {
+        public static double Add(double a) => a * Math.PI * 2;
+    }
+}
